76b314aa0c6612cbbd24d3c001773db66e7c06ec
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-15 16:14:09 -0300

    Backport armor filter config screen types to 1.16.5

--- src/main/java/net/pedroksl/advanced_ae/client/gui/QuantumArmorFilterConfigScreen.java
+++ src/main/java/net/pedroksl/advanced_ae/client/gui/QuantumArmorFilterConfigScreen.java
@@ -1,7 +1,7 @@
 package net.pedroksl.advanced_ae.client.gui;
 
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.text.ITextComponent;
 import net.pedroksl.advanced_ae.gui.QuantumArmorFilterConfigMenu;
 
 import appeng.client.gui.style.ScreenStyle;
@@ -9,7 +9,10 @@ import appeng.client.gui.style.ScreenStyle;
 public class QuantumArmorFilterConfigScreen extends QuantumArmorFilterScreen<QuantumArmorFilterConfigMenu> {
 
     public QuantumArmorFilterConfigScreen(
-            QuantumArmorFilterConfigMenu menu, Inventory playerInventory, Component title, ScreenStyle style) {
+            QuantumArmorFilterConfigMenu menu,
+            PlayerInventory playerInventory,
+            ITextComponent title,
+            ScreenStyle style) {
         super(menu, playerInventory, title, style);
     }
 }
