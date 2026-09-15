337bf1176ceb22fb94022163a88be1e22f6ac090
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-15 16:14:45 -0300

    Backport armor filter screen to AE2 8 GUI

--- src/main/java/net/pedroksl/advanced_ae/client/gui/QuantumArmorFilterScreen.java
+++ src/main/java/net/pedroksl/advanced_ae/client/gui/QuantumArmorFilterScreen.java
@@ -1,65 +1,27 @@
 package net.pedroksl.advanced_ae.client.gui;
 
-import java.util.ArrayList;
-
-import org.jetbrains.annotations.NotNull;
-
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.Slot;
-import net.pedroksl.advanced_ae.common.items.upgrades.UpgradeType;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.text.ITextComponent;
 import net.pedroksl.advanced_ae.gui.QuantumArmorFilterConfigMenu;
 
-import appeng.api.stacks.GenericStack;
 import appeng.client.gui.AEBaseScreen;
 import appeng.client.gui.implementations.AESubScreen;
 import appeng.client.gui.style.ScreenStyle;
-import appeng.core.localization.ButtonToolTips;
-import appeng.core.localization.Tooltips;
 
+/**
+ * 1.16.5 armor filter screen.
+ *
+ * <p>The modern amount-editing tooltip depends on GenericStack and GuiGraphics,
+ * neither of which exists in AE2 8.4.x / Minecraft 1.16.5. Keep the filter
+ * screen and its return navigation on the historical AE2 GUI API; quantity
+ * editing will be restored together with the filter container storage model.</p>
+ */
 public class QuantumArmorFilterScreen<M extends QuantumArmorFilterConfigMenu> extends AEBaseScreen<M> {
 
-    public QuantumArmorFilterScreen(M menu, Inventory playerInventory, Component title, ScreenStyle style) {
+    public QuantumArmorFilterScreen(
+            M menu, PlayerInventory playerInventory, ITextComponent title, ScreenStyle style) {
         super(menu, playerInventory, title, style);
 
-        AESubScreen.addBackButton(menu, "back", widgets);
-    }
-
-    @Override
-    public boolean mouseClicked(double xCoord, double yCoord, int btn) {
-        if (this.menu.upgradeType == UpgradeType.AUTO_STOCK) {
-            assert this.minecraft != null;
-
-            if (this.minecraft.options.keyPickItem.matchesMouse(btn)) {
-                Slot slot = this.findSlot(xCoord, yCoord);
-                if (this.isValidSlot(slot)) {
-                    this.menu.openAmountMenu(slot.index);
-                }
-            }
-        }
-
-        return super.mouseClicked(xCoord, yCoord, btn);
-    }
-
-    @Override
-    protected void renderTooltip(@NotNull GuiGraphics guiGraphics, int x, int y) {
-        if (this.menu.getCarried().isEmpty() && this.isValidSlot(this.hoveredSlot)) {
-            ArrayList<Component> itemTooltip =
-                    new ArrayList<>(this.getTooltipFromContainerItem(this.hoveredSlot.getItem()));
-            GenericStack unwrapped = GenericStack.fromItemStack(this.hoveredSlot.getItem());
-            if (unwrapped != null) {
-                itemTooltip.add(Tooltips.getAmountTooltip(ButtonToolTips.Amount, unwrapped));
-            }
-
-            itemTooltip.add(Tooltips.getSetAmountTooltip());
-            this.drawTooltip(guiGraphics, x, y, itemTooltip);
-        } else {
-            super.renderTooltip(guiGraphics, x, y);
-        }
-    }
-
-    private boolean isValidSlot(Slot slot) {
-        return slot != null && slot.isActive() && slot.hasItem() && this.menu.isConfigSlot(slot);
+        new AESubScreen(menu.getHost()).addBackButton("back", widgets);
     }
 }
