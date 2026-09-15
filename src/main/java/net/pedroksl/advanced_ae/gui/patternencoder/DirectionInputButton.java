f2db6d7cee1692eee3182ce647ae6db88c387934
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-15 15:21:10 -0300

    Backport direction input switch to Java 8

--- src/main/java/net/pedroksl/advanced_ae/gui/patternencoder/DirectionInputButton.java
+++ src/main/java/net/pedroksl/advanced_ae/gui/patternencoder/DirectionInputButton.java
@@ -60,15 +60,22 @@ public class DirectionInputButton extends Button {
     }
 
     public Direction getDirection() {
-        return switch (index) {
-            case 1 -> Direction.NORTH;
-            case 2 -> Direction.EAST;
-            case 3 -> Direction.SOUTH;
-            case 4 -> Direction.WEST;
-            case 5 -> Direction.UP;
-            case 6 -> Direction.DOWN;
-            default -> null;
-        };
+        switch (index) {
+            case 1:
+                return Direction.NORTH;
+            case 2:
+                return Direction.EAST;
+            case 3:
+                return Direction.SOUTH;
+            case 4:
+                return Direction.WEST;
+            case 5:
+                return Direction.UP;
+            case 6:
+                return Direction.DOWN;
+            default:
+                return null;
+        }
     }
 
     @Override
