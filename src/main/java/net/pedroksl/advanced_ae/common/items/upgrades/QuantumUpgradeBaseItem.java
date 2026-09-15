ccf61e19fc4892d9ef89a03885641a0f576922b4
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-15 16:24:16 -0300

    Backport quantum upgrade item tooltip to 1.16.5

--- src/main/java/net/pedroksl/advanced_ae/common/items/upgrades/QuantumUpgradeBaseItem.java
+++ src/main/java/net/pedroksl/advanced_ae/common/items/upgrades/QuantumUpgradeBaseItem.java
@@ -2,18 +2,19 @@ package net.pedroksl.advanced_ae.common.items.upgrades;
 
 import java.util.List;
 
-import org.jetbrains.annotations.Nullable;
+import javax.annotation.Nullable;
 
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.Level;
-import net.pedroksl.advanced_ae.common.definitions.AAEText;
-import net.pedroksl.advanced_ae.common.items.armors.QuantumArmorBase;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
 
-import appeng.core.localization.Tooltips;
-
+/**
+ * Quantum armor upgrade item using the Minecraft 1.16 tooltip API.
+ */
 public class QuantumUpgradeBaseItem extends Item {
     private final UpgradeType type;
 
@@ -32,21 +33,9 @@ public class QuantumUpgradeBaseItem extends Item {
     }
 
     @Override
-    public void appendHoverText(
-            ItemStack stack, @Nullable Level level, List<Component> lines, TooltipFlag tooltipFlag) {
-        super.appendHoverText(stack, level, lines, tooltipFlag);
-        if (type == UpgradeType.EMPTY) {
-            lines.add(type.getTooltip());
-            return;
-        }
-
-        lines.add(AAEText.QuantumUpgradeTooltip.text().withStyle(Tooltips.NUMBER_TEXT));
-        lines.add(type.getTooltip());
-        List<QuantumArmorBase> list = QuantumArmorBase.upgradeAvailableFor(type);
-        lines.add(Component.empty());
-        lines.add(AAEText.UpgradeTooltip.text().withStyle(Tooltips.NORMAL_TOOLTIP_TEXT));
-        for (QuantumArmorBase equip : list) {
-            lines.add(Component.translatable(equip.getDescriptionId()).withStyle(Tooltips.NORMAL_TOOLTIP_TEXT));
-        }
+    public void addInformation(
+            ItemStack stack, @Nullable World world, List<ITextComponent> lines, ITooltipFlag tooltipFlag) {
+        super.addInformation(stack, world, lines, tooltipFlag);
+        lines.add(new TranslationTextComponent(type.getTranslationKey()).mergeStyle(TextFormatting.GRAY));
     }
 }
