55d135924c0a6f121182704c4d479feb472ab7dd
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-15 16:16:59 -0300

    Backport import export bus menu to AE2 8

--- src/main/java/net/pedroksl/advanced_ae/gui/ImportExportBusMenu.java
+++ src/main/java/net/pedroksl/advanced_ae/gui/ImportExportBusMenu.java
@@ -1,32 +1,27 @@
 package net.pedroksl.advanced_ae.gui;
 
-import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.MenuType;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.ContainerType;
 import net.pedroksl.advanced_ae.common.definitions.AAEMenus;
 import net.pedroksl.advanced_ae.common.parts.ImportExportBusPart;
 
-import appeng.core.definitions.AEItems;
-import appeng.menu.implementations.UpgradeableMenu;
+import appeng.container.implementations.IOBusContainer;
 
-public class ImportExportBusMenu extends UpgradeableMenu<ImportExportBusPart> {
+/**
+ * 1.16.5 container for the import/export bus.
+ */
+public class ImportExportBusMenu extends IOBusContainer {
 
-    public ImportExportBusMenu(int id, Inventory ip, ImportExportBusPart host) {
-        this(AAEMenus.IMPORT_EXPORT_BUS.get(), id, ip, host);
+    public ImportExportBusMenu(int id, PlayerInventory playerInventory, ImportExportBusPart host) {
+        this(AAEMenus.IMPORT_EXPORT_BUS.get(), id, playerInventory, host);
     }
 
-    public ImportExportBusMenu(
-            MenuType<? extends ImportExportBusMenu> menuType, int id, Inventory ip, ImportExportBusPart host) {
-        super(menuType, id, ip, host);
+    protected ImportExportBusMenu(
+            ContainerType<?> containerType, int id, PlayerInventory playerInventory, ImportExportBusPart host) {
+        super(containerType, id, playerInventory, host);
     }
 
-    @Override
-    protected void setupConfig() {
-        this.addExpandableConfigSlots((this.getHost()).getConfig(), 2, 9, 5);
-    }
-
-    @Override
-    public boolean isSlotEnabled(int idx) {
-        int upgrades = this.getUpgrades().getInstalledUpgrades(AEItems.CAPACITY_CARD);
-        return upgrades > idx;
+    public ImportExportBusPart getHost() {
+        return (ImportExportBusPart) this.getUpgradeable();
     }
 }
