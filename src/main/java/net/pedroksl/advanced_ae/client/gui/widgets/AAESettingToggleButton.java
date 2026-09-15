fc3e06836876aafaaf8443f8774fda62abf309b7
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-15 18:09:53 -0300

    Backport AAE setting toggle button to AE2 8 widgets

--- src/main/java/net/pedroksl/advanced_ae/client/gui/widgets/AAESettingToggleButton.java
+++ src/main/java/net/pedroksl/advanced_ae/client/gui/widgets/AAESettingToggleButton.java
@@ -1,37 +1,104 @@
 package net.pedroksl.advanced_ae.client.gui.widgets;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
-import net.pedroksl.advanced_ae.AdvancedAE;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.pedroksl.advanced_ae.api.AAESetting;
 import net.pedroksl.advanced_ae.api.AAESettings;
 import net.pedroksl.advanced_ae.api.ShowQuantumCrafters;
 import net.pedroksl.advanced_ae.common.definitions.AAEText;
-import net.pedroksl.ae2addonlib.client.widgets.AddonSettingToggleButton;
 
-import appeng.api.config.Setting;
 import appeng.api.config.YesNo;
+import appeng.client.gui.AEBaseScreen;
+import appeng.client.gui.Icon;
+import appeng.client.gui.widgets.ITooltip;
+import appeng.util.EnumCycler;
 
-public class AAESettingToggleButton<T extends Enum<T>> extends AddonSettingToggleButton<T> {
-    public AAESettingToggleButton(
-            Setting<T> setting, T val, AddonSettingToggleButton.IHandler<AddonSettingToggleButton<T>> onPress) {
-        super(setting, val, t -> true, onPress);
+/**
+ * AE2 8.x compatible toggle button for AdvancedAE-owned settings.
+ */
+public class AAESettingToggleButton<T extends Enum<T>> extends Button implements ITooltip {
+    private static final Map<EnumPair, ButtonAppearance> APPEARANCES = new HashMap<>();
+
+    private final AAESetting<T> buttonSetting;
+    private final IHandler<AAESettingToggleButton<T>> onPress;
+    private final EnumSet<T> validValues;
+    private T currentValue;
+
+    @FunctionalInterface
+    public interface IHandler<B extends AAESettingToggleButton<?>> {
+        void handle(B button, boolean backwards);
     }
 
     public AAESettingToggleButton(
-            Setting<T> setting,
+            AAESetting<T> setting, T val, IHandler<AAESettingToggleButton<T>> onPress) {
+        this(setting, val, t -> true, onPress);
+    }
+
+    public AAESettingToggleButton(
+            AAESetting<T> setting,
             T val,
             Predicate<T> isValidValue,
-            AddonSettingToggleButton.IHandler<AddonSettingToggleButton<T>> onPress) {
-        super(setting, val, isValidValue, onPress);
+            IHandler<AAESettingToggleButton<T>> onPress) {
+        super(0, 0, 16, 16, StringTextComponent.EMPTY, AAESettingToggleButton::onPress);
+        this.buttonSetting = setting;
+        this.currentValue = val;
+        this.onPress = onPress;
+
+        EnumSet<T> validValues = EnumSet.allOf(val.getDeclaringClass());
+        validValues.removeIf(isValidValue.negate());
+        validValues.removeIf(value -> !setting.getValues().contains(value));
+        this.validValues = validValues;
+
+        EnumPair key = new EnumPair(setting, val);
+        if (!APPEARANCES.containsKey(key)) {
+            registerAppearances();
+        }
+        if (!APPEARANCES.containsKey(key)) {
+            throw new IllegalArgumentException("No button appearance for setting " + setting.getName() + "=" + val);
+        }
     }
 
-    public static <T extends Enum<T>> AAESettingToggleButton<T> serverButton(Setting<T> setting, T val) {
-        return AddonSettingToggleButton.serverButton(setting, val, AdvancedAE.MOD_ID, AAESettingToggleButton::new);
+    /**
+     * Temporary client-side cycling factory used while the addon setting packet
+     * is being backported away from ae2addonlib. The button no longer requires
+     * the missing library; server synchronization is handled in the networking
+     * port rather than hidden inside the widget.
+     */
+    public static <T extends Enum<T>> AAESettingToggleButton<T> serverButton(AAESetting<T> setting, T val) {
+        return new AAESettingToggleButton<>(setting, val, (button, backwards) ->
+                button.set(button.getNextValue(backwards)));
     }
 
-    @Override
-    protected void registerAppearances() {
+    private static void onPress(Button button) {
+        if (button instanceof AAESettingToggleButton) {
+            ((AAESettingToggleButton<?>) button).triggerPress();
+        }
+    }
+
+    private void triggerPress() {
+        boolean backwards = false;
+        Screen currentScreen = Minecraft.getInstance().currentScreen;
+        if (currentScreen instanceof AEBaseScreen) {
+            backwards = ((AEBaseScreen<?>) currentScreen).isHandlingRightClick();
+        }
+        onPress.handle(this, backwards);
+    }
+
+    private void registerAppearances() {
         registerApp(AAEIcon.ME_EXPORT_ON, AAESettings.ME_EXPORT, YesNo.YES, AAEText.MeExport, AAEText.MeExportOn);
         registerApp(AAEIcon.ME_EXPORT_OFF, AAESettings.ME_EXPORT, YesNo.NO, AAEText.MeExport, AAEText.MeExportOff);
         registerApp(
@@ -88,5 +155,116 @@ public class AAESettingToggleButton<T extends Enum<T>> extends AddonSettingToggl
                 YesNo.NO,
                 AAEText.RegulateCategory,
                 AAEText.RegulateOff);
+    }
+
+    private static <E extends Enum<E>> void registerApp(
+            AAEIcon icon, AAESetting<E> setting, E value, AAEText title, AAEText hint) {
+        List<ITextComponent> tooltip = new ArrayList<>(2);
+        tooltip.add(title.text());
+        tooltip.add(hint.text());
+        APPEARANCES.put(new EnumPair(setting, value), new ButtonAppearance(icon, tooltip));
+    }
+
+    private ButtonAppearance getAppearance() {
+        return APPEARANCES.get(new EnumPair(buttonSetting, currentValue));
+    }
+
+    @Override
+    public void renderWidget(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+        if (!visible) {
+            return;
+        }
+
+        ButtonAppearance appearance = getAppearance();
+        RenderSystem.disableDepthTest();
+        RenderSystem.enableBlend();
+        Icon.TOOLBAR_BUTTON_BACKGROUND.getBlitter().dest(x, y).blit(matrixStack, getBlitOffset());
+        if (appearance != null) {
+            appearance.icon.getBlitter().dest(x, y).blit(matrixStack, getBlitOffset());
+        }
+        RenderSystem.enableDepthTest();
+    }
+
+    public AAESetting<T> getSetting() {
+        return buttonSetting;
+    }
+
+    public T getCurrentValue() {
+        return currentValue;
+    }
+
+    public void set(T value) {
+        if (buttonSetting.isValidValue(value)) {
+            currentValue = value;
+        }
+    }
+
+    public T getNextValue(boolean backwards) {
+        return EnumCycler.rotateEnum(currentValue, backwards, validValues);
+    }
+
+    @Override
+    public List<ITextComponent> getTooltipMessage() {
+        ButtonAppearance appearance = getAppearance();
+        return appearance == null ? Collections.emptyList() : appearance.tooltip;
+    }
+
+    @Override
+    public int getTooltipAreaX() {
+        return x;
+    }
+
+    @Override
+    public int getTooltipAreaY() {
+        return y;
+    }
+
+    @Override
+    public int getTooltipAreaWidth() {
+        return width;
+    }
+
+    @Override
+    public int getTooltipAreaHeight() {
+        return height;
+    }
+
+    @Override
+    public boolean isTooltipAreaVisible() {
+        return visible;
+    }
+
+    private static final class EnumPair {
+        private final AAESetting<?> setting;
+        private final Enum<?> value;
+
+        private EnumPair(AAESetting<?> setting, Enum<?> value) {
+            this.setting = setting;
+            this.value = value;
+        }
+
+        @Override
+        public int hashCode() {
+            return setting.hashCode() ^ value.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof EnumPair)) {
+                return false;
+            }
+            EnumPair other = (EnumPair) obj;
+            return other.setting == setting && other.value == value;
+        }
+    }
+
+    private static final class ButtonAppearance {
+        private final AAEIcon icon;
+        private final List<ITextComponent> tooltip;
+
+        private ButtonAppearance(AAEIcon icon, List<ITextComponent> tooltip) {
+            this.icon = icon;
+            this.tooltip = tooltip;
+        }
     }
 }
