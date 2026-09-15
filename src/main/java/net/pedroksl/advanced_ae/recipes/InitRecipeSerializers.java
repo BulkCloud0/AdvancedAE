57a7816ff679e6dd295bcbb89ab170b3065d4cb3
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-15 13:47:22 -0300

    Port recipe serializer registration to Forge 1.16.5

--- src/main/java/net/pedroksl/advanced_ae/recipes/InitRecipeSerializers.java
+++ src/main/java/net/pedroksl/advanced_ae/recipes/InitRecipeSerializers.java
@@ -1,19 +1,22 @@
 package net.pedroksl.advanced_ae.recipes;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.pedroksl.advanced_ae.AdvancedAE;
 
 public final class InitRecipeSerializers {
+    private static final DeferredRegister<IRecipeSerializer<?>> SERIALIZERS =
+            DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, AdvancedAE.MOD_ID);
+
+    static {
+        SERIALIZERS.register("reaction", () -> ReactionChamberRecipeSerializer.INSTANCE);
+    }
 
     private InitRecipeSerializers() {}
 
-    public static void init(IForgeRegistry<RecipeSerializer<?>> registry) {
-        register(registry, ReactionChamberRecipe.TYPE_ID, ReactionChamberRecipeSerializer.INSTANCE);
-    }
-
-    private static void register(
-            IForgeRegistry<RecipeSerializer<?>> registry, ResourceLocation id, RecipeSerializer<?> serializer) {
-        registry.register(id, serializer);
+    public static void register(IEventBus eventBus) {
+        SERIALIZERS.register(eventBus);
     }
 }
