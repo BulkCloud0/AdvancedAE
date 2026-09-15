6488b83e46e11d1e10289a8df1fba8cfa5a075a4
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-15 16:17:08 -0300

    Backport import export bus part baseline to AE2 8

--- src/main/java/net/pedroksl/advanced_ae/common/parts/ImportExportBusPart.java
+++ src/main/java/net/pedroksl/advanced_ae/common/parts/ImportExportBusPart.java
@@ -1,87 +1,55 @@
 package net.pedroksl.advanced_ae.common.parts;
 
-import org.jetbrains.annotations.Nullable;
-
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.vector.Vector3d;
 import net.pedroksl.advanced_ae.AdvancedAE;
 import net.pedroksl.advanced_ae.common.definitions.AAEMenus;
-import net.pedroksl.advanced_ae.common.helpers.FilteredImportStackTransferContext;
 
-import appeng.api.behaviors.StackImportStrategy;
-import appeng.api.networking.IGrid;
 import appeng.api.parts.IPartModel;
+import appeng.container.ContainerLocator;
+import appeng.container.ContainerOpener;
 import appeng.core.AppEng;
-import appeng.core.definitions.AEItems;
 import appeng.items.parts.PartModels;
-import appeng.menu.ISubMenu;
-import appeng.menu.MenuOpener;
-import appeng.menu.locator.MenuLocators;
 import appeng.parts.PartModel;
 import appeng.parts.automation.ExportBusPart;
-import appeng.parts.automation.StackWorldBehaviors;
 
+/**
+ * 1.16.5 compatibility baseline for the combined import/export bus.
+ *
+ * <p>The modern import strategy API does not exist in AE2 8.4.x. Export
+ * behavior is inherited from the historical ExportBusPart while the import
+ * half is restored in a later storage-API pass.</p>
+ */
 public class ImportExportBusPart extends ExportBusPart {
 
     public static final ResourceLocation MODEL_BASE = AdvancedAE.makeId("part/import_export_bus_part");
 
     @PartModels
-    public static final IPartModel MODELS_OFF = new PartModel(MODEL_BASE, AppEng.makeId("part/export_bus_off"));
+    public static final IPartModel MODELS_OFF = new PartModel(
+            MODEL_BASE, new ResourceLocation(AppEng.MOD_ID, "part/export_bus_off"));
 
     @PartModels
-    public static final IPartModel MODELS_ON = new PartModel(MODEL_BASE, AppEng.makeId("part/export_bus_on"));
+    public static final IPartModel MODELS_ON = new PartModel(
+            MODEL_BASE, new ResourceLocation(AppEng.MOD_ID, "part/export_bus_on"));
 
     @PartModels
-    public static final IPartModel MODELS_HAS_CHANNEL =
-            new PartModel(MODEL_BASE, AppEng.makeId("part/export_bus_has_channel"));
-
-    @Nullable
-    private StackImportStrategy importStrategy;
+    public static final IPartModel MODELS_HAS_CHANNEL = new PartModel(
+            MODEL_BASE, new ResourceLocation(AppEng.MOD_ID, "part/export_bus_has_channel"));
 
     public ImportExportBusPart(ItemStack partItem) {
         super(partItem);
     }
 
-    public StackImportStrategy getImportStrategy() {
-        if (this.importStrategy == null) {
-            var self = this.getHost().getBlockEntity();
-            var fromPos = self.getBlockPos().relative(this.getSide());
-            var fromSide = getSide().getOpposite();
-            importStrategy = StackWorldBehaviors.createImportFacade((ServerLevel) getLevel(), fromPos, fromSide);
-        }
-        return this.importStrategy;
-    }
-
     @Override
-    protected boolean doBusWork(IGrid grid) {
-        var exportWork = super.doBusWork(grid);
-
-        var strategy = getImportStrategy();
-
-        var context = new FilteredImportStackTransferContext(
-                grid.getStorageService(), grid.getEnergyService(), this.source, getOperationsPerTick(), getFilter());
-
-        context.setInverted(this.isUpgradedWith(AEItems.INVERTER_CARD));
-        strategy.transfer(context);
-
-        var importWork = context.hasDoneWork();
-
-        return exportWork || importWork;
-    }
-
-    protected MenuType<?> getMenuType() {
-        return AAEMenus.IMPORT_EXPORT_BUS.get();
-    }
-
-    public void returnToMainMenu(Player player, ISubMenu subMenu) {
-        MenuOpener.open(AAEMenus.IMPORT_EXPORT_BUS.get(), player, MenuLocators.forPart(this));
-    }
-
-    public ItemStack getMainMenuIcon() {
-        return this.getPartItem().asItem().getDefaultInstance();
+    public boolean onPartActivate(PlayerEntity player, Hand hand, Vector3d pos) {
+        if (!isRemote()) {
+            ContainerOpener.openContainer(
+                    AAEMenus.IMPORT_EXPORT_BUS.get(), player, ContainerLocator.forPart(this));
+        }
+        return true;
     }
 
     @Override
