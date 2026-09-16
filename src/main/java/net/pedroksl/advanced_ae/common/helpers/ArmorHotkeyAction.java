c691408f2a3636167bc931be4b910cbe59e9c87c
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-16 16:05:56 -0300

    port: convert armor hotkey action to Java 8

--- src/main/java/net/pedroksl/advanced_ae/common/helpers/ArmorHotkeyAction.java
+++ src/main/java/net/pedroksl/advanced_ae/common/helpers/ArmorHotkeyAction.java
@@ -9,7 +9,11 @@ import net.minecraft.world.level.ItemLike;
 
 import appeng.api.features.HotkeyAction;
 
-public record ArmorHotkeyAction(Predicate<ItemStack> locatable, Opener opener) implements HotkeyAction {
+/** Java 8 equivalent of the upstream record used by the 1.20.x codebase. */
+public final class ArmorHotkeyAction implements HotkeyAction {
+    private final Predicate<ItemStack> locatable;
+    private final Opener opener;
+
     public ArmorHotkeyAction(ItemLike item, Opener opener) {
         this((stack) -> stack.is(item.asItem()), opener);
     }
@@ -21,17 +25,15 @@ public record ArmorHotkeyAction(Predicate<ItemStack> locatable, Opener opener) i
 
     @Override
     public boolean run(Player player) {
-        var items = player.getArmorSlots();
+        Iterable<ItemStack> items = player.getArmorSlots();
         int i = 0;
-        for (var item : items) {
-            if (this.locatable.test(item)) {
-                if (opener.open(player, Inventory.INVENTORY_SIZE + i, item)) {
-                    return true;
-                }
+        for (ItemStack item : items) {
+            if (this.locatable.test(item)
+                    && this.opener.open(player, Inventory.INVENTORY_SIZE + i, item)) {
+                return true;
             }
             i++;
         }
-
         return false;
     }
 
@@ -45,6 +47,6 @@ public record ArmorHotkeyAction(Predicate<ItemStack> locatable, Opener opener) i
 
     @FunctionalInterface
     public interface Opener {
-        boolean open(Player var1, int inventorySlot, ItemStack stack);
+        boolean open(Player player, int inventorySlot, ItemStack stack);
     }
 }
