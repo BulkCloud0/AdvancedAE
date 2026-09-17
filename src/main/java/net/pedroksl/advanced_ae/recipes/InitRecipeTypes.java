11251f1111929b4867ce6c04eca793c70c39bd2b
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-17 11:21:32 -0300

    fix: make recipe type registry Java 8 compatible

--- src/main/java/net/pedroksl/advanced_ae/recipes/InitRecipeTypes.java
+++ src/main/java/net/pedroksl/advanced_ae/recipes/InitRecipeTypes.java
@@ -3,13 +3,22 @@ package net.pedroksl.advanced_ae.recipes;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.var;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class InitRecipeTypes {
-    private record ToRegister(RecipeType<?> recipeType, ResourceLocation id) {}
+    private static final class ToRegister {
+        private final RecipeType<?> recipeType;
+        private final ResourceLocation id;
+
+        private ToRegister(RecipeType<?> recipeType, ResourceLocation id) {
+            this.recipeType = recipeType;
+            this.id = id;
+        }
+    }
 
     private static final List<ToRegister> toRegister = new ArrayList<>();
 
@@ -20,8 +29,8 @@ public class InitRecipeTypes {
     }
 
     public static void init(IForgeRegistry<RecipeType<?>> registry) {
-        for (var toRegister : toRegister) {
-            registry.register(toRegister.id, toRegister.recipeType);
+        for (var entry : toRegister) {
+            registry.register(entry.id, entry.recipeType);
         }
     }
 }
