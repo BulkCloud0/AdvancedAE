44ae58af0baf0db40ad48ff1ad7a952e95063f0b
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-16 22:11:37 -0300

    fix: port item tracking packet to Forge 1.16.5

--- src/main/java/net/pedroksl/advanced_ae/network/packet/ItemTrackingPacket.java
+++ src/main/java/net/pedroksl/advanced_ae/network/packet/ItemTrackingPacket.java
@@ -2,10 +2,10 @@ package net.pedroksl.advanced_ae.network.packet;
 
 import java.util.UUID;
 
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.PacketBuffer;
 import net.pedroksl.ae2addonlib.network.AddonPacket;
 
 public class ItemTrackingPacket extends AddonPacket {
@@ -14,32 +14,33 @@ public class ItemTrackingPacket extends AddonPacket {
     private final int entityId;
     private final int pickupDelay;
 
-    public ItemTrackingPacket(FriendlyByteBuf stream) {
+    public ItemTrackingPacket(PacketBuffer stream) {
         this.thrower = stream.readUUID();
         this.entityId = stream.readInt();
         this.pickupDelay = stream.readInt();
     }
 
     public ItemTrackingPacket(ItemEntity item) {
-        this.thrower = item.thrower;
+        this.thrower = item.getThrower();
         this.entityId = item.getId();
-        this.pickupDelay = item.pickupDelay;
+        this.pickupDelay = item.hasPickUpDelay() ? 10 : 0;
     }
 
     @Override
-    public void write(FriendlyByteBuf stream) {
+    public void write(PacketBuffer stream) {
         stream.writeUUID(this.thrower);
         stream.writeInt(this.entityId);
         stream.writeInt(this.pickupDelay);
     }
 
     @Override
-    public void clientPacketData(Player player) {
-        if (player != null && player.level() != null) {
-            Entity entity = player.level().getEntity(this.entityId);
+    public void clientPacketData(PlayerEntity player) {
+        if (player != null && player.level != null) {
+            Entity entity = player.level.getEntity(this.entityId);
             if (entity instanceof ItemEntity) {
-                ((ItemEntity) entity).thrower = this.thrower;
-                ((ItemEntity) entity).setPickUpDelay(this.pickupDelay);
+                ItemEntity item = (ItemEntity) entity;
+                item.setThrower(this.thrower);
+                item.setPickUpDelay(this.pickupDelay);
             }
         }
     }
