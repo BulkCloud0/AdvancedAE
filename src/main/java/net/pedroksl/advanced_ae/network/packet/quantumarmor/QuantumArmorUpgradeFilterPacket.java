c1b6655a68efbb51e9ca66cd2107b4bf15b1350f
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-17 10:33:49 -0300

    port: migrate quantum filter packet to 1.16.5

--- src/main/java/net/pedroksl/advanced_ae/network/packet/quantumarmor/QuantumArmorUpgradeFilterPacket.java
+++ src/main/java/net/pedroksl/advanced_ae/network/packet/quantumarmor/QuantumArmorUpgradeFilterPacket.java
@@ -3,8 +3,8 @@ package net.pedroksl.advanced_ae.network.packet.quantumarmor;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.network.PacketBuffer;
 import net.pedroksl.advanced_ae.common.items.upgrades.UpgradeType;
 import net.pedroksl.advanced_ae.gui.QuantumArmorConfigMenu;
 import net.pedroksl.ae2addonlib.network.AddonPacket;
@@ -16,7 +16,7 @@ public class QuantumArmorUpgradeFilterPacket extends AddonPacket {
     private final UpgradeType upgradeType;
     private final List<GenericStack> filter;
 
-    public QuantumArmorUpgradeFilterPacket(FriendlyByteBuf stream) {
+    public QuantumArmorUpgradeFilterPacket(PacketBuffer stream) {
         upgradeType = stream.readEnum(UpgradeType.class);
 
         int size = stream.readInt();
@@ -32,7 +32,8 @@ public class QuantumArmorUpgradeFilterPacket extends AddonPacket {
         this.filter = filter;
     }
 
-    public void write(FriendlyByteBuf stream) {
+    @Override
+    public void write(PacketBuffer stream) {
         stream.writeEnum(upgradeType);
 
         stream.writeInt(filter.size());
@@ -42,7 +43,7 @@ public class QuantumArmorUpgradeFilterPacket extends AddonPacket {
     }
 
     @Override
-    public void serverPacketData(ServerPlayer serverPlayer) {
+    public void serverPacketData(ServerPlayerEntity serverPlayer) {
         if (serverPlayer.containerMenu instanceof QuantumArmorConfigMenu) {
             QuantumArmorConfigMenu menu = (QuantumArmorConfigMenu) serverPlayer.containerMenu;
             menu.openFilterConfigScreen(upgradeType, filter);
