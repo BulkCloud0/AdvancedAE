4317b441c696b8253baba401ded2601ab7984d26
BulkCloud0 <78863204+BulkCloud0@users.noreply.github.com>
2026-09-16 22:11:57 -0300

    fix: port clear quantum crafter packet to Forge 1.16.5

--- src/main/java/net/pedroksl/advanced_ae/network/packet/ClearQuantumCrafterTerminalPacket.java
+++ src/main/java/net/pedroksl/advanced_ae/network/packet/ClearQuantumCrafterTerminalPacket.java
@@ -1,21 +1,22 @@
 package net.pedroksl.advanced_ae.network.packet;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.PacketBuffer;
 import net.pedroksl.advanced_ae.client.gui.QuantumCrafterTermScreen;
 import net.pedroksl.ae2addonlib.network.AddonPacket;
 
 public class ClearQuantumCrafterTerminalPacket extends AddonPacket {
 
-    public ClearQuantumCrafterTerminalPacket(FriendlyByteBuf stream) {}
+    public ClearQuantumCrafterTerminalPacket(PacketBuffer stream) {}
 
     public ClearQuantumCrafterTerminalPacket() {}
 
-    public void write(FriendlyByteBuf data) {}
+    @Override
+    public void write(PacketBuffer data) {}
 
     @Override
-    public void clientPacketData(Player player) {
+    public void clientPacketData(PlayerEntity player) {
         if (Minecraft.getInstance().screen instanceof QuantumCrafterTermScreen) {
             QuantumCrafterTermScreen<?> screen = (QuantumCrafterTermScreen<?>) Minecraft.getInstance().screen;
             screen.clear();
